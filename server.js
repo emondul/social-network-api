e94f2ffa43e08928418a6a146650a0d1f19b36ba
Emma Mondul <emondul@gmail.com>
2022-05-07 19:43:13 -0700

    setting up files and folders

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = 3001;
+const app = express();
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server for ${activity} running on port ${PORT}!`);
+    });
+  });
