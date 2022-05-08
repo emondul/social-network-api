db7726b2bb02a02eece9e2503f2ab22bb2bb8b7f
Emma Mondul <emondul@gmail.com>
2022-05-08 10:02:47 -0700

    finsihed controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -38,15 +38,58 @@ module.exports = {
       })
     },
     updateThought(req, res) {
-       
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+          )
+            .then((thought) =>
+              !thought
+                ? res.status(404).json({ message: 'No thought with this id!' })
+                : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     deleteThought(req, res) {
-        
+        Thought.findOneAndRemove({ _id: req.params.thoughtId })
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No such thought exists' })
+              : Thought.findOneAndUpdate(
+                  { users: req.params.thoughtId },
+                  { $pull: { users: req.params.thoughtId } },
+                  { new: true }
+                )
+          )
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
     },
     addReaction(req, res) {
-      
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+          )
+            .then((thought) =>
+              !thought
+                ? res.status(404).json({ message: 'No thought with this id!' })
+                : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     removeReaction(req, res) {
-      
+        Thought.findOneAndUpdate(
+            { _id: req.params.reactionId },
+            { $pull: { tags: { tagId: req.params.tagId } } },
+            { runValidators: true, new: true }
+          )
+            .then((application) =>
+              !application
+                ? res.status(404).json({ message: 'No application with this id!' })
+                : res.json(application)
+            )
+            .catch((err) => res.status(500).json(err));
     },
 }
